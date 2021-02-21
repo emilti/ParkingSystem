8097f5bd254cfa3bc3e8b4d640e01faedc329667
emilti <e.o.tishinov@gmail.com>
2021-02-21 18:29:58 +0200

    Date format added to the Entered parking date column

--- client/src/Components/vehicle/index.js
+++ client/src/Components/vehicle/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Styles from './index.module.css'
+import Moment from 'moment'
 
 const Vehicle = ({registrationNumber, enterParkingDate, categoryName, discountPercentage, dueAmount, index}) => {
    return (<div className={Styles.row}>
         <span className={[Styles.cell, index == 0 ? Styles.topRow : "", index % 2 != 0 ? Styles.evenRow : ""].join(" ")}>{registrationNumber} </span>
-        <span className={[Styles.cell, index == 0 ? Styles.topRow : "", index % 2 != 0 ? Styles.evenRow : ""].join(" ")}>{enterParkingDate} </span>
+        <span className={[Styles.cell, index == 0 ? Styles.topRow : "", index % 2 != 0 ? Styles.evenRow : ""].join(" ")}>{Moment(String(enterParkingDate)).format('MM/DD/YYYY HH:mm:ss')}</span>
         <span className={[Styles.cell, index == 0 ? Styles.topRow : "", index % 2 != 0 ? Styles.evenRow : ""].join(" ")}>{categoryName} </span>
         <span className={[Styles.cell, index == 0 ? Styles.topRow : "", index % 2 != 0 ? Styles.evenRow : ""].join(" ")}>{discountPercentage != null ? discountPercentage + "%" : "-" } </span>
         <span className={[Styles.cell, index == 0 ? Styles.topRow : "", index % 2 != 0 ? Styles.evenRow : ""].join(" ")}>{dueAmount}</span></div>)
