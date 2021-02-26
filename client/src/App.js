17459817e7977107aa5787dd06669a378d6217b8
emilti <e.o.tishinov@gmail.com>
2021-02-26 18:39:07 +0200

    Header links added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/header';
 import Vehicles from './Components/vehicles';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>Parking system</h1>
+      <BrowserRouter>
+        <Header/>
+      </BrowserRouter>
       <Vehicles/>
     </div>
   );
