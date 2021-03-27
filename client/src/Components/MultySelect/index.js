df8349ecf6fc0f682bf08e3566a6639861f9a5ae
emilti <e.o.tishinov@gmail.com>
2021-03-27 21:58:55 +0200

    Filers styled, added date range filter

--- client/src/Components/MultySelect/index.js
+++ client/src/Components/MultySelect/index.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react'
+import {Form, Col } from 'react-bootstrap'
 
-const MultySelect = ({field, collection}) => {
+const MultySelect = ({field, collection, onChangeMultyselect, selectedCategories}) => {
     return (
-        <select name={field} multiple>
-        {
-            collection.map((el, i) => {
-                return <option value={el.value}>{el.label}</option>
-            })}
-        </select>
+
+        <Col sm={2} className="my-1"> 
+            <Form.Label>{field}</Form.Label>
+            <Form.Control as="select" multiple value={selectedCategories} onChange={e => onChangeMultyselect(e)}>
+            {collection.map((el, i) => {
+                    return <option key={i} value={el.value}>{el.label}</option>
+                })}
+            </Form.Control>
+        </Col>   
     )
 }
 export default MultySelect
